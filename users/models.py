c25091f334aa3be7d18cd8c3cdaeb11441214c4d
Natalia Bazhina <nataliaagapova27@yandex.ru>
2024-12-10 00:13:18 +0300

    add model UserManager and create superuser

--- users/models.py
+++ users/models.py
@@ -1,17 +1,39 @@
 from datetime import datetime
-
+from django.contrib.auth.models import BaseUserManager, UserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from lms.models import Course, Lesson
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("У пользователя должна быть почта")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Суперпользователь должен иметь is_staff=True.")
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Суперпользователь должен иметь is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     username = None
-
     email = models.EmailField(
         unique=True, verbose_name="почта", help_text="Введите почту"
     )
+
     phone = models.CharField(
         max_length=35,
         verbose_name="телефон",
@@ -37,12 +59,15 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
+    objects = UserManager()
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
 
     def __str__(self):
         return self.email
+
 
 
 class Payment(models.Model):
